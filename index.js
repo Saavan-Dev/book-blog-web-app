5f40c3f26ad20863eb2b6d12c597b724f048bacd
Saavan-Dev <crosslpdgajjar@gmail.com>
2024-04-06 20:18:27 +0530

    modified env with connection string

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const app = new express();
 const port = 3000;
 env.config();
 const pool = new pg.Pool({
-  // connectionString: process.env.POSTGRES_URL ,
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  connectionString: process.env.POSTGRES_URL ,
+  // user: process.env.PG_USER,
+  // host: process.env.PG_HOST,
+  // database: process.env.PG_DATABASE,
+  // password: process.env.PG_PASSWORD,
+  // port: process.env.PG_PORT
 })
 
 app.set('view engine', 'ejs');
